f844c0c16da64720a4a1bd1d91e0bc59ef608b86
mohsen-gis <60162675+mohsen-gis@users.noreply.github.com>
2020-07-18 12:15:40 -0500

    Updates

---
+++ precache-manifest.9bb89b5f02ce26fd4d8ed6a4cf986a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868f01697d2f758f47c7a9c43e9a1aee",
+    "url": "/carlson_covid/index.html"
+  },
+  {
+    "revision": "206183cffe62241c44eb",
+    "url": "/carlson_covid/static/css/main.c416f8e3.chunk.css"
+  },
+  {
+    "revision": "1c912c6f062ff5f07158",
+    "url": "/carlson_covid/static/js/2.85e6b163.chunk.js"
+  },
+  {
+    "revision": "229e68bea92a75bf6415f2a76dfb20a2",
+    "url": "/carlson_covid/static/js/2.85e6b163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206183cffe62241c44eb",
+    "url": "/carlson_covid/static/js/main.ef53f0d2.chunk.js"
+  },
+  {
+    "revision": "b9758cd32e1ea21f5f94",
+    "url": "/carlson_covid/static/js/runtime-main.9e6a09ef.js"
+  },
+  {
+    "revision": "76537688c59647f97ef2c89c807c0532",
+    "url": "/carlson_covid/static/media/logo.76537688.png"
+  }
+]);
